ba96040d311a8fb2c18567713000cd798e391035
Stanislav Bashkyrtsev <stanislav.bashkyrtsev@elsci.io>
2022-12-28 12:30:33 +0200

    Add childrenOf() to step through variations of words in descending order

--- src/main/java/io/elsci/multinomial/QuickSyllableGenerator.java
+++ src/main/java/io/elsci/multinomial/QuickSyllableGenerator.java
@@ -10,12 +10,14 @@ public class QuickSyllableGenerator implements SyllableGenerator {
 
     public Iterator<Word> generate(Alphabet a, int numberOfSymbols) {
         int[] numbers = new int[a.probabilities.length];
-        for (int i=0; i < a.probabilities.length; i++)
+        for (int i = 0; i < a.probabilities.length; i++)
             // TODO: will this always round correctly?
             numbers[i] = (int) Math.round(a.probabilities[i] * numberOfSymbols);
 
-        double syllableProb = multinomMassFunction(a, numbers);
+        double syllableProb = multinomMassFunction(numbers, a.probabilities);
 
+        // 1. Write an algorithm to enumerate all children of a given element: `numbers[][] childrenOf(numbers[])`
+        // 2. Use PriorityQueue<Word, Comparator> and custom Iterator<Word>
         List<Word> words = new ArrayList<>();
         words.add(new Word(new SymbolSet(a, numbers), syllableProb));
 
@@ -32,17 +34,61 @@ public class QuickSyllableGenerator implements SyllableGenerator {
         // 4x0x1 -> (5x0x0, 4x1x0(!), 3x1x1, 3x0x2)
         return words.iterator();
     }
+
+    /**
+     * It returns elements with Manhattan Distance = 2. We need this to find a symbolSet with the next highest
+     * probability - and it's going to differ from the "parent" symbolSet by 1 symbol transfer. Since that symbol
+     * will be transferred to the other one, we'll have differences in 2 positions - which effectively results in
+     * Manhattan Distance = 2.
+     * <p>
+     * It is supposed to be used in {@link #generate(Alphabet, int)} to enumerate all children of a given symbolSet.
+     *
+     * @param symbolSet is an array where the index of each element represents an index of the symbol of an alphabet
+     *                  and its value is the number of this symbol occurrences. It's very similar to {@link SymbolSet},
+     *                  just a different structure.
+     * @return all children, which means all possible variants of changes to symbolSet with the step of Manhattan
+     * distance = 2
+     */
+    static int[][] childrenOf(int[] symbolSet) {
+        int numbersLength = symbolSet.length;
+        if (numbersLength == 1)
+            return new int[0][];
+        int f = 0;
+        for (int num : symbolSet) {
+            if (num != 0)
+                f++;
+        }
+        int[][] result = new int[f * (numbersLength - 1)][numbersLength];
+        for (int i = 0; i < result.length; i++)
+            result[i] = symbolSet.clone();
+        int i = 0;
+        outer:
+        while (true) {
+            for (int j = 0; j < numbersLength; j++) {
+                for (int e = 0; e < numbersLength; e++) {
+                    if (e != j && result[i][e] != 0) {
+                        result[i][j]++;
+                        result[i][e]--;
+                        if (++i >= result.length)
+                            break outer;
+                    }
+                }
+            }
+        }
+        return result;
+    }
+
     /**
      * Uses <a href="https://en.wikipedia.org/wiki/Multinomial_distribution">Gamma functions</a>
      */
-    private static double multinomMassFunction(Alphabet a, int[] numbers) {
+    private static double multinomMassFunction(int[] numbers, double[] probabilities) {
         double numerator = gamma(sum(numbers) + 1);
         double denominator = 1;
         for (int number : numbers)
             denominator *= gamma(number + 1);
         double prob = 1;
-        for (int i = 0; i < a.probabilities.length; i++)
-            prob *= Math.pow(a.probabilities[i], numbers[i]);
+        for (int i = 0; i < probabilities.length; i++)
+            prob *= Math.pow(probabilities[i], numbers[i]);
         return prob * numerator / denominator;
     }
 
--- src/test/java/io/elsci/multinomial/QuickSyllableGeneratorTest.java
+++ src/test/java/io/elsci/multinomial/QuickSyllableGeneratorTest.java
@@ -7,8 +7,7 @@ import java.util.LinkedHashMap;
 import java.util.Map;
 
 import static io.elsci.multinomial.AssertUtils.assertWordsEqual;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.*;
 
 public class QuickSyllableGeneratorTest {
     @Test
@@ -74,6 +73,49 @@ public class QuickSyllableGeneratorTest {
 
         Word expected = new Word(new SymbolSet(map(a.getSymbol(0), 1, a.getSymbol(1), 2, a.getSymbol(2), 3)), 0.135);
         assertWordsEqual(expected, it.next());
+    }
+
+    @Test
+    public void childrenOf_returnsElementsWithManhattanDistanceOf2() {
+        int[] numbers = {2, 3, 1};
+        int[][] actual = QuickSyllableGenerator.childrenOf(numbers);
+        assertEquals(6, actual.length);
+        assertArrayEquals(new int[] {3, 2, 1}, actual[0]);
+        assertArrayEquals(new int[] {3, 3, 0}, actual[1]);
+        assertArrayEquals(new int[] {1, 4, 1}, actual[2]);
+        assertArrayEquals(new int[] {2, 4, 0}, actual[3]);
+        assertArrayEquals(new int[] {1, 3, 2}, actual[4]);
+        assertArrayEquals(new int[] {2, 2, 2}, actual[5]);
+
+    }
+
+    @Test
+    public void childrenOf_neverReturnsNegativesValues() {
+        int[] numbers = {2, 3, 0};
+        int[][] actual = QuickSyllableGenerator.childrenOf(numbers);
+        assertEquals(4, actual.length);
+        assertArrayEquals(new int[] {3, 2, 0}, actual[0]);
+        assertArrayEquals(new int[] {1, 4, 0}, actual[1]);
+        assertArrayEquals(new int[] {1, 3, 1}, actual[2]);
+        assertArrayEquals(new int[] {2, 2, 1}, actual[3]);
+
+    }
+
+    @Test
+    public void childrenOf_neverReturnsNegativesValues_whenZeroIsNotAtTheEnd() {
+        int[] numbers = {0, 0, 4};
+        int[][] actual = QuickSyllableGenerator.childrenOf(numbers);
+        assertEquals(2, actual.length);
+        assertArrayEquals(new int[] {1, 0, 3}, actual[0]);
+        assertArrayEquals(new int[] {0, 1, 3}, actual[1]);
+    }
+
+    @Test
+    public void childrenOfArrayWithOneElement_returnsEmptyArray() {
+        int[] numbers = {1};
+        int[][] actual = QuickSyllableGenerator.childrenOf(numbers);
+        assertEquals(0, actual.length);
+
     }
 
     private static Iterator<Word> generate(Alphabet a, int syllableLength) {
