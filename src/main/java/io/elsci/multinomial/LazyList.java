25d3fa296818b1737a0c9661dae25efcc9620a84
Anna Ponomarova <anutapontera@gmail.com>
2023-01-05 19:08:01 +0100

    Class LazyList<T> is created
    It is like a list, but also contains an iterator. So we have access to any element when we need, but we don't store all elements at first.
    It will be used in WordIterator to iterate alphabet syllables.

---
+++ src/main/java/io/elsci/multinomial/LazyList.java
@@ -0,0 +1,37 @@
+package io.elsci.multinomial;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+class LazyList<T> {
+    private final Iterator<T> it;
+    private final List<T> cache = new ArrayList<>();
+
+    LazyList(Iterator<T> it) {
+        this.it = it;
+    }
+
+    public T get(int idx) {
+        if (idx >= cache.size())
+            iterateToOrThrow(idx);
+        return cache.get(idx);
+    }
+    public boolean has(int idx) {
+        return iterateTo(idx);
+    }
+
+    private boolean iterateTo(int idx) {
+        for (int i = cache.size(); i <= idx; i++) {
+            if (!it.hasNext())
+                return false;
+            cache.add(it.next());
+        }
+        return true;
+    }
+    private void iterateToOrThrow(int idx) {
+        if (!iterateTo(idx))
+            throw new IndexOutOfBoundsException(
+                    "There are only " + cache.size() + " elements in the list, can't retrieve #" + idx);
+    }
+}
