01b453519f235a2538678310d8206a3b241d5088
Stanislav Bashkyrtsev <stanislav.bashkirtsev@gmail.com>
2023-04-29 12:41:51 -0400

    Add docs to WordSpec

--- src/main/java/io/elsci/multinomialselection/WordSpec.java
+++ src/main/java/io/elsci/multinomialselection/WordSpec.java
@@ -2,12 +2,15 @@ package io.elsci.multinomialselection;
 
 import java.util.Map;
 
+/**
+ * Requirements for word generation: how many symbols can a word have, which alphabets should we sample from.
+ */
 public class WordSpec {
-    /**
-     * How many Symbols of each Alphabet we have (want to pull)
-     */
     final Map<Alphabet, Integer> numberOfSymbols;
 
+    /**
+     * @param numberOfSymbols how many Symbols of each Alphabet we have (want to pull)
+     */
     public WordSpec(Map<Alphabet, Integer> numberOfSymbols) {
         this.numberOfSymbols = numberOfSymbols;
     }
